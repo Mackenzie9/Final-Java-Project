2a17397669886de86348526cc1b22abaa3bbb7fb
AdamRushdy1 <105012946+AdamRushdy1@users.noreply.github.com>
2022-05-17 11:43:38 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,3 +1,4 @@
+
 package game;
 
 import java.io.*;
@@ -12,59 +13,65 @@ public class Character {
   int xPos;
   int yPos;
   
+  int width;
+  int height;
+  
   BufferedImage img;
 
-  
-  public Character() {
+  public Character(int w, int h) {
     xPos = 0;
     yPos = 0;
-
+    
+    width = w;
+    height = h;
+    
+    
     
     try {
-  	  img = ImageIO.read(new File("Smile.png"));
+  	  img = ImageIO.read(new File("Smile2.png"));
     } catch (IOException e) {
     	e.printStackTrace();
     }
 
     
   }
-
-  
-  public Character(String fileName) {
-    xPos = 0;
-    yPos = 0;
-
-    try {
-  	  img = ImageIO.read(new File(fileName));
-    } catch (IOException e) {
-    	e.printStackTrace();
-    }
-  }
-
   
   public Image getImage() {
 	  return img;
   }
   
-  public void move() {
+  
+
+  /*public void move() {
     xPos += 40;
     yPos += 40;
-  }
+  }*/
 
   public void moveLeft(){
     xPos -= 40;
+    if (xPos < 0) {
+    	xPos = 0;
+    }
+    
   }
-  
   public void moveRight() {
     xPos += 40;
+    if (xPos >= width) {
+    	xPos = width - img.getWidth();
+    }
   }
-  
   public void moveDown() {
     yPos += 40;
+    if (yPos >= height) {
+    	yPos = height - img.getHeight();
+    }
   }
 
   public void moveUp(){
     yPos -= 40;
+    if (yPos < 0) {
+    	yPos = 0;
+    }
   }
   
   public int getXPos(){
@@ -82,5 +89,8 @@ public class Character {
   public void setXPos(int x) {
 	    xPos = x;
   }
+  
+  
+  
   
 }
