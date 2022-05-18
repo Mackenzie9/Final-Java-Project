69af1e1232ddf38caa298e7d071d3b8c045a915f
AdamRushdy1 <105012946+AdamRushdy1@users.noreply.github.com>
2022-05-18 11:48:18 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,19 +1,52 @@
 package game;
 
 public class Board {
-	private Tile[][] tiles;
-	  public Board() {
-	    this.tiles = new Tile[2][2];
+  private Tile[][] tiles;
+  
+  //private Character character;
+  
+  public Board() {
+    this.tiles = new Tile[15][15];  //for basic board
+    for (int r = 0; r < tiles.length; r++) {
+      for (int c = 0; c < tiles[r].length; c++) {
+        if (r == 0 || c == 0 || c == 14 || r == 14) {
+          tiles[r][c] = new Tile(1);
+        } else {
+          tiles[r][c] = new Tile(0);
+        }
+      }
+    }
+    //tiles[6][14] = new Tile(4);
+  }
+  
+  
+  public Board(int[][] arr) {    // for custom board
+	  this.tiles = new Tile[15][15];
+	  for (int r = 0; r < tiles.length; r++) {
+		  for (int c = 0; c < tiles[0].length; c++) {
+			  tiles[r][c] = new Tile(arr[r][c]);
+		  }
 	  }
-	  public int numberOpen() {
-	    int count = 0; 
+  }
+  
+  
+  
+  
+
+  public int numberOpen() {
+	  int count = 0; 
 	    for (int r = 0; r < tiles.length; r++) {
 	      for (int c = 0; c < tiles[r].length; c++) {
-	        if (tiles[r][c].getAvailability()) {
+	        if (tiles[r][c].getCollision()) {
 	          count++;
 	        }
 	      }
-	    }
-	    return count;
-	  }
+      }
+    return count;
+   }
+
+  public Tile getTile(int r, int c) {
+    return this.tiles[r][c];
+  }
+    
 }
