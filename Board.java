3cce78efb09347cf55700f0f768ef8503b450e4a
BlueYellow-Green <101900880+BlueYellow-Green@users.noreply.github.com>
2022-06-03 13:12:56 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,46 +1,47 @@
 package game;
-public class Board {
-	  private Tile[][] tiles;
-	  
-	  
-	  public Board() {
-	    this.tiles = new Tile[15][15];
-	    for (int r = 0; r < tiles.length; r++) {
-	      for (int c = 0; c < tiles[r].length; c++) {
-	        if (r == 0 || c == 0 || c == 14 || r == 14) {
-	          tiles[r][c] = new Tile(1);
-	        } else {
-	          tiles[r][c] = new Tile(0);
-	        }
-	      }
-	    }
-	    tiles[7][14] = new Tile(3);
-	  }
 
-	  public Board(int[][] arr) {    // for custom board
-		  this.tiles = new Tile[15][15];
-		  for (int r = 0; r < tiles.length; r++) {
-			  for (int c = 0; c < tiles[0].length; c++) {
-				  tiles[r][c] = new Tile(arr[r][c]);
-			  }
+public class Board {
+  private Tile[][] tiles;
+  
+  
+  public Board() {
+    this.tiles = new Tile[15][15];
+    for (int r = 0; r < tiles.length; r++) {
+      for (int c = 0; c < tiles[r].length; c++) {
+        if (r == 0 || c == 0 || c == 14 || r == 14) {
+          tiles[r][c] = new Tile(1);
+        } else {
+          tiles[r][c] = new Tile(0);
+        }
+      }
+    }
+    tiles[7][14] = new Tile(3);
+  }
+
+  public Board(int[][] arr) {    // for custom board
+	  this.tiles = new Tile[15][15];
+	  for (int r = 0; r < tiles.length; r++) {
+		  for (int c = 0; c < tiles[0].length; c++) {
+			  tiles[r][c] = new Tile(arr[r][c]);
 		  }
 	  }
+  }
 
 
-	  public int numberOpen() {
-		  int count = 0; 
-		  for (int r = 0; r < tiles.length; r++) {
-		    for (int c = 0; c < tiles[r].length; c++) {
-		      if (tiles[r][c].getCollision()) {
-		        count++;
-	        }
-	      }
-	    }
-	    return count;
-	  }
+  public int numberOpen() {
+	  int count = 0; 
+	  for (int r = 0; r < tiles.length; r++) {
+	    for (int c = 0; c < tiles[r].length; c++) {
+	      if (tiles[r][c].getCollision()) {
+	        count++;
+        }
+      }
+    }
+    return count;
+  }
 
-	  public Tile getTile(int r, int c) {
-	    return this.tiles[r][c];
-	  }
-	    
-	}
+  public Tile getTile(int r, int c) {
+    return this.tiles[r][c];
+  }
+    
+}
